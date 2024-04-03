e66278465060a58d6fa776ceb6beba3fc5a9b23a
jgressier <jeremie.gressier@isae.fr>
2024-04-03 22:36:14 +0200

    debug pytest action

--- tests/test_icalimport.py
+++ tests/test_icalimport.py
@@ -14,7 +14,6 @@ def test_init():
     assert (project._end-timedelta(days=1)).date().isoformat() == ir._default_enddate
 
 def test_open():
-    print(sys.argv, os.getcwd())
     prjname = "my_original_name"
     path = "examples/ProjectA"
     project = ir.Project(name=prjname, folder=path)
